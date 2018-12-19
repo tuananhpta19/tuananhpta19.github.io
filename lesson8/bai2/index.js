7428bd870a3e99851cb2c71e1a3c66f40e4eb331
unknown <tuananh.pta19@gmail.com>
2018-12-19 19:17:52 +0700

    the project

--- lesson8/bai2/index.js
+++ lesson8/bai2/index.js
@@ -1,7 +1,14 @@
 var tong = 0;
-for (var i = 0; i < 10; i++) {
-	if(i%2==0){
+var dem = 0;
+for (var i = 0; i < 100; i++) {
+	if(i%2==0 ){
 		tong+=i;
+		dem++;
+	}
+	if(dem==10){
+		break;
 	}
 }
+
+
 alert(tong);
